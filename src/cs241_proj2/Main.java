9f2824e6ea22cf3d1ecad9d973a3f9e8fffbdf49
B. Haussmann <bjhaussmann@gmail.com>
2018-02-04 15:46:46 -0800

    Main created and Project 2 initialized

---
+++ src/cs241_proj2/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package cs241_proj2;
+
+/**
+ * @author bjhau
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
